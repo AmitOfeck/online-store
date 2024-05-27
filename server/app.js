5490f49c1c168c2684cf3dd1bc3e7f6ff1a0d4ab
Amit Ofeck <Amitofeck1@gmail.com>
2024-05-27 19:58:40 +0300

    env setup
    setup of enviroments (local:8080 , test:8082)' defauls is local. see the scripts in package json

--- server/app.js
+++ server/app.js
@@ -4,17 +4,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const articles = require('./routes/article');
 
-//require('custom-env').env(process.env.NODE_ENV, './config');
+require('custom-env').env(process.env.NODE_ENV, './config');
 
-mongoose.connect("mongodb://localhost:27017/news", 
-    {   useNewUrlParser: true, 
-        useUnifiedTopology: true });
-
-/*
 mongoose.connect(process.env.CONNECTION_STRING, 
                 {   useNewUrlParser: true, 
                     useUnifiedTopology: true });
-                    */
+                    
 
 var app = express();
 app.use(cors());
@@ -23,5 +18,4 @@ app.use(express.json());
 
 app.use('/articles', articles);
 
-app.listen(8080);
-//app.listen(process.env.PORT);
+app.listen(process.env.PORT);
