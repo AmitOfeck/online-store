20176ee4f120f0e77ecaff74e2f3ae899c4ffea3
Amit Ofeck <Amitofeck1@gmail.com>
2024-05-27 18:55:47 +0300

    working mongo db
    working mongo db setup on localhost 8080 port

--- server/app.js
+++ server/app.js
@@ -4,11 +4,17 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const articles = require('./routes/article');
 
-require('custom-env').env(process.env.NODE_ENV, './config');
+//require('custom-env').env(process.env.NODE_ENV, './config');
 
+mongoose.connect("mongodb://localhost:27017/news", 
+    {   useNewUrlParser: true, 
+        useUnifiedTopology: true });
+
+/*
 mongoose.connect(process.env.CONNECTION_STRING, 
                 {   useNewUrlParser: true, 
                     useUnifiedTopology: true });
+                    */
 
 var app = express();
 app.use(cors());
@@ -17,4 +23,5 @@ app.use(express.json());
 
 app.use('/articles', articles);
 
-app.listen(process.env.PORT);
+app.listen(8080);
+//app.listen(process.env.PORT);
