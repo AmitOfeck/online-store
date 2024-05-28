fa633d18c0dc79b352214d615e3cd11d2224d4a6
Amit Ofeck <Amitofeck1@gmail.com>
2024-05-28 13:04:48 +0300

    handle 404 error on getUser
    use try catch

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,12 +11,18 @@ const getUsers = async (req, res) => {
 };
 
 const getUser = async (req, res) => {
+  try {
     const user = await userService.getUserById(req.params.id);
+    
     if (!user) {
-        return res.status(404).json({ errors: ['User not found'] });
+      return res.status(404).json({ errors: ['User not found'] });
     }
 
     res.json(user);
+  } catch (error) {
+    //console.error('Error fetching user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const updateUser = async (req, res) => {
