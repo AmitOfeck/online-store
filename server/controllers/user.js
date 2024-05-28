c7233e85e93cc9ab67bd2a8eff62cb306b940414
Amit Ofeck <Amitofeck1@gmail.com>
2024-05-28 13:12:52 +0300

    add isValid function on users
    add isValid function to handle 404 errors' on create user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,36 @@
 const userService = require('../services/user');
 
+const isValid = (req, res, fields) => {
+  for (let field of fields) {
+    if (!req.body[field]) {
+      res.status(400).json({ message: `${field} is required` });
+      return false;
+    }
+  }
+  return true;
+};
+
 const createUser = async (req, res) => {
-    const newUser = await userService.createUser(req.body.email, req.body.password, req.body.type, req.body.firstName, req.body.lastName, req.body.streetAddress, req.body.city);
+  try {
+    const requiredFields = ['email', 'password', 'type', 'firstName', 'lastName', 'streetAddress', 'city'];
+
+    if (!isValid(req, res, requiredFields)) return;
+
+    const newUser = await userService.createUser(
+      req.body.email,
+      req.body.password,
+      req.body.type,
+      req.body.firstName,
+      req.body.lastName,
+      req.body.streetAddress,
+      req.body.city
+    );
+
     res.json(newUser);
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const getUsers = async (req, res) => {
