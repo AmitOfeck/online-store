30ed9f6a47cba6205cdfaaf7a7e2ec835a2e8818
Amit Ofeck <Amitofeck1@gmail.com>
2024-05-28 12:20:12 +0300

    add try catch to update product
    handle 404 errors

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -20,22 +20,51 @@ const getProduct = async (req, res) => {
 };
 
 const updateProduct = async (req, res) => {
-
-    if (!req.body.category) {  res.status(400).json({  message: "Category is required", }); }
-    if (!req.body.name) {  res.status(400).json({  message: "Name is required", }); }
-    if (!req.body.supplierId) {  res.status(400).json({  message: "Supplier Id is required", }); }
-    if (!req.body.manufacturer) {  res.status(400).json({  message: "Manufacturer is required", }); }
-    if (!req.body.price) {  res.status(400).json({  message: "Price is required", }); }
-    if (!req.body.currentStock) {  res.status(400).json({  message: "CurrentStock is required", }); }
-    //if (!req.body.image) {  res.status(400).json({  message: "Image is required", }); } //Maybe we will do it required
-    
-    const product = await productService.updateProduct(req.params.id, req.body.category, req.body.name, req.body.supplierId, req.body.manufacturer, req.body.price, req.body.currentStock, req.body.image);
-    if (!product) {
-      return res.status(404).json({ errors: ['Product not found'] });
-    }
+    try {
+      if (!req.body.category) {
+        return res.status(400).json({ message: "Category is required" });
+      }
+      if (!req.body.name) {
+        return res.status(400).json({ message: "Name is required" });
+      }
+      if (!req.body.supplierId) {
+        return res.status(400).json({ message: "Supplier Id is required" });
+      }
+      if (!req.body.manufacturer) {
+        return res.status(400).json({ message: "Manufacturer is required" });
+      }
+      if (!req.body.price) {
+        return res.status(400).json({ message: "Price is required" });
+      }
+      if (!req.body.currentStock) {
+        return res.status(400).json({ message: "CurrentStock is required" });
+      }
+      // if (!req.body.image) {
+      //   return res.status(400).json({ message: "Image is required" });
+      // } // Maybe we will do it required
   
-    res.json(product);
+      const product = await productService.updateProduct(
+        req.params.id,
+        req.body.category,
+        req.body.name,
+        req.body.supplierId,
+        req.body.manufacturer,
+        req.body.price,
+        req.body.currentStock,
+        req.body.image
+      );
+  
+      if (!product) {
+        return res.status(404).json({ errors: ['Product not found'] });
+      }
+  
+      res.json(product);
+    } catch (error) {
+      console.error('Error updating product:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
   };
+
 
   const deleteProduct = async (req, res) => {
     const product = await productService.deleteProduct(req.params.id);
