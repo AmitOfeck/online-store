83c351863b672d8881a69003c968d53f00826a40
Amit Ofeck <Amitofeck1@gmail.com>
2024-05-29 23:33:47 +0300

    add premissions per type for products
    getAllProducts - admin, customer, supplier
    createProduct - admin, supplier
    getProductById - admin, customer , supplier
    updateProduct - admin, supplier
    deleteProduct - admin

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,14 +1,16 @@
 const express = require('express');
 var router = express.Router();
 const productsController = require('../controllers/product');
+const verifyToken = require('../middleware/authMiddleware');
+const validateType = require('../middleware/validateType');
 
 router.route('/')
-    .get(productsController.getProducts)
-    .post(productsController.createProduct);
+    .get(verifyToken, validateType(['admin' , 'customer' , 'supplier']), productsController.getProducts)
+    .post(verifyToken, validateType(['admin' , 'supplier']), productsController.createProduct);
 
 router.route('/:id')
-    .get(productsController.getProduct)
-    .patch(productsController.updateProduct)
-    .delete(productsController.deleteProduct);
+    .get(verifyToken, validateType(['admin' , 'customer' , 'supplier']), productsController.getProduct)
+    .patch(verifyToken, validateType(['admin' , 'supplier']), productsController.updateProduct)
+    .delete(verifyToken, validateType(['admin']), productsController.deleteProduct);
 
 module.exports = router;
