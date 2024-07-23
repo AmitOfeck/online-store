0f15da0d1ac1adef98925ca9c22177a8802f0781
Amit Ofeck <Amitofeck1@gmail.com>
2024-07-23 12:51:37 +0300

    make calculateBill an external function
    make calculateBill an external function , in middeleware dir

--- server/services/order.js
+++ server/services/order.js
@@ -1,17 +1,7 @@
 const Order = require('../models/order');
 const Product = require('../models/product'); 
+const calculateBill = require('../middleware/calculateBill'); 
 
-
-const calculateBill = async (items) => {
-    let total = 0;
-    for (const item of items) {
-        const product = await Product.findById(item.id);
-        if (product) {
-            total += product.price * item.quantity;
-        }
-    }
-    return total;
-};
 
 const createOrder = async (customerId, items, ordered, bill) => {
 
