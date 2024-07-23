1a0f060f89862625fea40c34454fae8846e3834a
Amit Ofeck <Amitofeck1@gmail.com>
2024-07-23 23:42:01 +0300

    js register form
    http://localhost:8080/auth/register , post

--- public/src/relog.js
+++ public/src/relog.js
@@ -21,3 +21,34 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
         alert('Login failed: ' + data.error);
     }
 });
+
+
+document.getElementById('registerForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const firstName = document.getElementById('registerFirstName').value;
+    const lastName = document.getElementById('registerLastName').value;
+    const email = document.getElementById('registerEmail').value;
+    const password = document.getElementById('registerPassword').value;
+    const type = document.getElementById('registerType').value;
+    const streetAddress = document.getElementById('registerStreet').value;
+    const city = document.getElementById('registerCity').value;
+
+    
+    const response = await fetch('http://localhost:8080/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password, type, firstName, lastName, streetAddress, city })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        alert('Registration successful');
+    } else {
+        alert('Registration failed: ' + data.error);
+    }
+    
+});
