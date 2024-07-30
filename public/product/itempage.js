f9d241c3f7875eebb0c3ab14a2349f46c83b00c7
OsherLulu <Osher1166@gmail.com>
2024-07-30 19:03:15 +0300

    shekels - dollars

--- public/product/itempage.js
+++ public/product/itempage.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('sub-category-link').textContent = productData.subCategory;
         document.getElementById('product-name').textContent = productData.name;
         document.getElementById('product-image').src = productData.image;
-        document.getElementById('product-price').textContent = `₪${productData.price}`;
+        document.getElementById('product-price').textContent = `$${productData.price}`;
         document.getElementById('product-manufacturer').textContent = productData.manufacturer;
         document.getElementById('product-weight').textContent = `${productData.weight} ${productData.weightUnit}`;
 
