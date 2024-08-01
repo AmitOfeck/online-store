bbd7143105608699d48f97a583b12830392343df
Galp2001 <galp2001@gmail.com>
2024-08-01 15:13:52 +0300

    change in navbar

--- public/src/homepage.js
+++ public/src/homepage.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
             <div class="nav-right">
               <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="bi bi-list"></i>
+                 Catalouge <i class="bi bi-list"></i>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end">
                   <a class="Cat">Menu</a>
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function() {
             <div class="nav-right">
               <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="bi bi-list"></i>
+                 Catalouge <i class="bi bi-list"></i>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end">
                   <a class="Cat">Menu</a>
@@ -119,7 +119,7 @@ document.addEventListener("DOMContentLoaded", function() {
             <div class="nav-right">
               <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="bi bi-list"></i>
+                  Catalouge <i class="bi bi-list"></i>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end">
                   <a class="Cat">Menu</a>
@@ -155,7 +155,7 @@ document.addEventListener("DOMContentLoaded", function() {
             <div class="nav-right">
               <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="bi bi-list"></i>
+                 Catalouge <i class="bi bi-list"></i>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end">
                   <a class="Cat">Menu</a>
